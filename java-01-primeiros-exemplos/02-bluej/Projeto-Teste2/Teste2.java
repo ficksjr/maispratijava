a754c9c09b57e8bd8b7dcc7c1d3696cf68e5276a
ficksjr <ficksjr@gmail.com>
2021-07-28 19:12:07 -0300

    aula 2

---
+++ java-01-primeiros-exemplos/02-bluej/Projeto-Teste2/Teste2.java
@@ -0,0 +1,19 @@
+
+/**
+ * Escreva uma descrição da classe Teste2 aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class Teste2
+{
+    //variavel com texto de saída
+    
+    
+    public static void main (String args[])
+    {
+        String saida;
+        saida = "Esse é um exemplo da segunda aula";
+        System.out.println(saida);
+    }
+}
---
+++ java-01-primeiros-exemplos/02-bluej/Projeto-Teste2/Teste21.java
@@ -0,0 +1,18 @@
+
+/**
+ * Escreva uma descrição da classe Teste21 aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class Teste21
+{
+    public static void main (String x[])
+    {
+        String saida = "";
+        
+        saida = x[0] + " - " + x[1];
+        
+        System.out.println(saida);
+    }
+}
