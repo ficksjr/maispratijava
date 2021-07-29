58944d0c2980aa5ec9e16ac0d206ea3ae07c1da5
ficksjr <ficksjr@gmail.com>
2021-07-29 11:03:42 -0300

    Create Teste3.java

---
+++ java-01-primeiros-exemplos/03-netbeans/Projeto3/src/Teste3.java
@@ -0,0 +1,8 @@
+
+public class Teste3 {
+    public static void main(String args[])
+    {
+        String saida = "Este é um teste do projeto 3 no Netbeans";
+        System.out.println(saida);
+    }
+}
